c31ab2e91b830c1650264ec6a8fb77812b116bf2
KapitanD <ddden97@gmail.com>
2021-03-23 11:10:31 +0300

    complete proxy

--- 01/proxy_server.lua
+++ 01/proxy_server.lua
@@ -1,11 +1,34 @@
-local yaml = require("yaml")
-local client = require("http.client")
-local fio = require("fio")
+local yaml = require('yaml')
+local http_client = require('http.client')
+local fio = require('fio')
 
-local fh, err = fio.open("config.yaml")
+local fh, err = fio.open('config.yaml')
 if err ~= nil then
     print("Can't open file: " .. err)
     return 1
 end
 
+local client = http_client.new()
 local config = yaml.decode(fh:read())
+local proxy_url = config.proxy.bypass.host .. ':' .. config.proxy.bypass.port
+
+
+local function proxy_handler(req)
+    local method = req:method()
+    local proxy_url_full = proxy_url..req:path()
+    if req:query() ~= '' then
+        proxy_url_full = proxy_url_full..'?'..req:query()
+    end
+    local body = req:read()
+    local headers = req:headers()
+    local resp = client:request(method, proxy_url_full, body, {headers = headers})
+    return resp
+end
+
+local router = require('http.router').new()
+router:route({path = '/.*', method = 'ANY'}, proxy_handler)
+
+local server = require('http.server').new('localhost', config.proxy.port)
+server:set_router(router)
+
+server:start()
---
+++ 01/test_server.lua
@@ -0,0 +1,15 @@
+-- Simle server
+local function hello()
+    return {
+        status = 200,
+        body = 'hello, world'
+    }
+end
+
+local router = require('http.router').new()
+router:route({ method = 'GET', path = '/hello' }, hello)
+
+local server = require('http.server').new('localhost', 9000)
+server:set_router(router)
+
+server:start()
