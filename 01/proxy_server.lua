59c2808e62d95621f926e83c3638e3e3af0ee6ec
KapitanD <ddden97@gmail.com>
2021-03-23 12:28:59 +0300

    some fixes

--- 01/proxy_server.lua
+++ 01/proxy_server.lua
@@ -15,18 +15,29 @@ local proxy_url = config.proxy.bypass.host .. ':' .. config.proxy.bypass.port
 
 local function proxy_handler(req)
     local method = req:method()
-    local proxy_url_full = proxy_url..req:path()
+    local proxy_url_full = 'http://'..proxy_url..req:path()
     if req:query() ~= '' then
         proxy_url_full = proxy_url_full..'?'..req:query()
     end
     local body = req:read()
+    if body == '' then
+        body = nil
+    end
     local headers = req:headers()
-    local resp = client:request(method, proxy_url_full, body, {headers = headers})
-    return resp
+    headers.host = config.proxy.bypass.host
+    print(method, proxy_url_full, body, headers.host)
+    local resp = client:request(method, proxy_url_full, body, {headers = headers, timeout = 1, follow_location = true})
+    print(resp.reason, resp.status, resp.body)
+    return {
+        status = resp.status,
+        headers = resp.headers,
+        body = resp.body
+    }
 end
 
 local router = require('http.router').new()
 router:route({path = '/.*', method = 'ANY'}, proxy_handler)
+router:route({path = '/', method = 'ANY'}, proxy_handler)
 
 local server = require('http.server').new('localhost', config.proxy.port)
 server:set_router(router)
