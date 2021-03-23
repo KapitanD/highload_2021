af064a969bec3b3fbb0d6e9755e8f391fab340b8
KapitanD <ddden97@gmail.com>
2021-03-23 09:31:35 +0300

    prepare

---
+++ 01/proxy_server.lua
@@ -0,0 +1,13 @@
+local yaml = require("yaml")
+local client = require("http.client")
+local fio = require("fio")
+
+local fh, err = fio.open("config.yaml")
+if err == nil then
+    print("Can't open file: " .. err)
+    return 1
+end
+
+local config = yaml.decode(fh:read())
+
+print(config)
